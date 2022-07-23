0f68ea53e76095254c489370808d714f589d7492
vishal-codes <itsvishal2417@gmail.com>
2022-07-23 22:58:53 +0530

    Country-wise analysis completed
    Signed-off-by: vishal-codes <itsvishal2417@gmail.com>

--- helper.py
+++ helper.py
@@ -85,3 +85,13 @@ def country_event_heatmap(df, country):
     pt = new_df.pivot_table(index='Sport', columns='Year', values='Medal', aggfunc='count').fillna(0).astype('int')
     return pt
 
+
+def most_successful_countrywise(df, country):
+    temp_df = df.dropna(subset=['Medal'])
+    temp_df = temp_df[temp_df['region'] == country]
+    x = temp_df['Name'].value_counts().reset_index().head(10).merge(df, left_on='index', right_on='Name', how='left')[
+        ['index', 'Name_x', 'Sport']].drop_duplicates('index')
+    x.rename(columns={'index': 'Name', 'Name_x': 'Medals'}, inplace=True)
+    return x
+
+
